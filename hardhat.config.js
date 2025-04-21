a35da9ea2aec8ecb888ad0c5157bee20dc951a25
codersjj <shanewestlife@outlook.com>
2025-04-21 22:01:32 +0800

    feat: Hardhat Setup - Smart Contract Lottery

---
+++ hardhat.config.js
@@ -0,0 +1,9 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+require("hardhat-deploy-ethers")
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+}
