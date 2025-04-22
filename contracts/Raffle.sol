b16af33f85b19de47ec869af2a813053678c08eb
codersjj <shanewestlife@outlook.com>
2025-04-22 22:27:52 +0800

    feat: Implementing Chainlink VRF - The Request

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -9,6 +9,7 @@
 pragma solidity ^0.8.7;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 error Raffle__NotEnoughETHEntered();
 
@@ -16,12 +17,27 @@ contract Raffle is VRFConsumerBaseV2Plus {
     // State Variables
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    bytes32 private immutable i_gasLane;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
     // Events
     event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
 
-    constructor(address vrfCoordinator, uint256 entranceFee) VRFConsumerBaseV2Plus(vrfCoordinator) {
+    constructor(
+        address vrfCoordinator,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -36,10 +52,24 @@ contract Raffle is VRFConsumerBaseV2Plus {
         emit RaffleEnter(msg.sender);
     }
 
-    function requestRandomWinner() external {
+    function requestRandomWinner(bool enableNativePayment) external {
         // Request the random number
         // Once we get it, do something with it
         // 2 transaction process
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_gasLane,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: enableNativePayment})
+                )
+            })
+        );
+
+        emit RequestedRaffleWinner(requestId);
     }
 
     function fulfillRandomWords(
