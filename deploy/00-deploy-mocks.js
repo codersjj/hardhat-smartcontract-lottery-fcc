084c9fe357a70388e7369e5f0bf99a3e688381e4
codersjj <shanewestlife@outlook.com>
2025-04-24 19:16:09 +0800

    feat: Mock Chainlink VRF Coordinator

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,15 +1,34 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
 const { developmentChains } = require("../helper-hardhat-config")
+
+// see:
+// https://docs.chain.link/vrf/v2-5/subscription/test-locally#deploy-vrfcoordinatorv2_5mock
+// https://github.com/smartcontractkit/hardhat-starter-kit/blob/main/test/unit/RandomNumberConsumer.spec.js
+const BASE_FEE = ethers.parseEther("0.001") // the base fee
+const GAS_PRICE = ethers.parseUnits("50", "gwei") // the gas price (in LINK tokens)
+const WEI_PER_UNIT_LINK = ethers.parseEther("0.01") // the current LINK/ETH price
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deployer } = await getNamedAccounts()
   const { deploy, log } = deployments
   // const chainId = network.config.chainId
   // console.log(network)
+
+  const args = [BASE_FEE, GAS_PRICE, WEI_PER_UNIT_LINK]
+
   if (!developmentChains.includes(network.name)) {
     return
   }
 
   log("Local network detected! Deploying mocks...")
   // deploy a mock VRF Coordinator...
+  await deploy("VRFCoordinatorV2_5Mock", {
+    from: deployer,
+    args,
+    log: true,
+  })
+  log("Mocks deployed!")
+  log("----------------------------------------")
 }
+
+module.exports.tags = ["all", "mocks"]
--- deploy/01-deploy-raffle.js
+++ deploy/01-deploy-raffle.js
@@ -1,10 +1,18 @@
-const { network } = require("hardhat")
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deployer } = await getNamedAccounts()
-  const { deploy, log } = deployments
+  const { deploy, log, get } = deployments
+  let vrfCoordinatorV2_5Address = null
 
-  const args = []
+  if (developmentChains.includes(network.name)) {
+    // const vrfCoordinatorV2_5Mock = await ethers.getContract("VRFCoordinatorV2_5Mock")
+    const vrfCoordinatorV2_5Mock = await get("VRFCoordinatorV2_5Mock")
+    vrfCoordinatorV2_5Address = vrfCoordinatorV2_5Mock.address
+  }
+
+  const args = [vrfCoordinatorV2_5Address]
 
   const raffle = await deploy("Raffle", {
     from: deployer,
